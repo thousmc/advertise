227355a2e3fba57a80e085b01b6bd80df7f7e02d
thou <nothou@proton.me>
2025-07-25 17:35:51 -0700

    change ip warning ad

--- ads/xyz-warning.py
+++ ads/xyz-warning.py
@@ -9,13 +9,13 @@ current_date = datetime.datetime.now()
 days_until_expiry = (expiry_date - current_date).days
 
 if days_until_expiry == 1:
-    message_text = f'ATTENTION: ".xyz" thousmc domains (e.g. play.thousmc.xyz) will stop working in {days_until_expiry} day on January 1st 2026! Please switch to "thousmc.net" or "play.thousmc.net!"'
+    message_text = f'ATTENTION: ".xyz" thousmc domains (e.g. play.thousmc.xyz) will stop working in {days_until_expiry} day! Please switch to "thousmc.net" or "play.thousmc.net!"'
 elif days_until_expiry <= 7:
-    message_text = f'WARNING: ".xyz" thousmc domains (e.g. play.thousmc.xyz) will stop working in {days_until_expiry} days on January 1st 2026! Please switch to "thousmc.net" or "play.thousmc.net!"'
+    message_text = f'WARNING: ".xyz" thousmc domains (e.g. play.thousmc.xyz) will stop working in {days_until_expiry} days! Please switch to "thousmc.net" or "play.thousmc.net!"'
 elif days_until_expiry <= 30:
-    message_text = f'Notice: ".xyz" thousmc domains (e.g. play.thousmc.xyz) will stop working in {days_until_expiry} days on January 1st 2026! Please switch to "thousmc.net" or "play.thousmc.net!"'
+    message_text = f'Notice: ".xyz" thousmc domains (e.g. play.thousmc.xyz) will stop working in {days_until_expiry} days! Please switch to "thousmc.net" or "play.thousmc.net!"'
 else:
-    message_text = f'Remember! ".xyz" thousmc domains (e.g. play.thousmc.xyz) will stop working in {days_until_expiry} days on January 1st 2026! Please switch to "thousmc.net" or "play.thousmc.net!"'
+    message_text = f'Remember! ".xyz" thousmc domains (e.g. play.thousmc.xyz) will stop working in {days_until_expiry} days! Please switch to "thousmc.net" or "play.thousmc.net!"'
 
 # Generate tellraw JSON
 tellraw_data = [
